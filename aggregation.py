513626fd574a5d2f6933bd7b3089488c3e92dcda
chefkoch24 <kuennecke-felix@web.de>
2023-02-23 14:09:58 +0100

    analyze label aggregations

--- aggregation.py
+++ aggregation.py
@@ -2,6 +2,9 @@
 import numpy as np
 import config
 import myutils as utils
+
+GLOBAL_NORMALIZE = True
+
 # Functions
 def mean_nonzero(col):
     return np.mean([c for c in col if c!=0])
@@ -16,14 +19,14 @@ def aggregate_soft_labels(soft_labels, mode:str):
     elif mode == 'sum':
         return np.sum(soft_labels, axis=0)
 
-def normalize(arr, t_min=0, t_max=1):
-    norm_arr = []
-    diff = t_max - t_min
-    diff_arr = max(arr) - min(arr)
-    for i in arr:
-        temp = (((i - min(arr))*diff)/diff_arr) + t_min
-        norm_arr.append(round(temp,1))
-    return norm_arr
+def normalize(sequence):
+    min_value = min(sequence)
+    max_value = max(sequence)
+    range_value = max_value - min_value
+    if range_value == 0:
+        range_value = 0.001
+    normalized_sequence = [(x - min_value) / range_value for x in sequence]
+    return normalized_sequence
 
 def extract_annotations(annotated_data, exclude_LFs=[]):
     labels = []
@@ -34,6 +37,28 @@ def extract_annotations(annotated_data, exclude_LFs=[]):
                 soft_labels.append(v)
         labels.append(soft_labels)
     return labels
+
+def global_normalize(data, average_outliers=False):
+    # finding min and max value
+    min_value = 100
+    max_value = -100
+    max_vals = []
+    min_vals = []
+    for d in data:
+        v_min = np.min(d)
+        v_max = np.max(d)
+        if v_min < min_value:
+            min_value = v_min
+            min_vals.append(min_value)
+        if v_max > max_value:
+            max_value = v_max
+            max_vals.append(max_value)
+    if average_outliers:
+        min_value = np.average(min_vals)
+        max_value = np.average(max_vals)
+    range_val = max_value - min_value
+    data_norm = [[(l - min_value) / range_val for l in d] for d in data]
+    return data_norm
 
 # Load data
 def _main():
@@ -49,8 +74,11 @@ def _main():
             silver_labels = []
             for a in data:
                 y = aggregate_soft_labels(a, mode)
-                y = normalize(y)
+                if not GLOBAL_NORMALIZE:
+                    y = normalize(y)
                 silver_labels.append(y)
+            if GLOBAL_NORMALIZE:
+                silver_labels = global_normalize(silver_labels, average_outliers=False)
             if i == 0:
                 silver_label_train = silver_labels
             elif i == 1:
@@ -62,6 +90,8 @@ def _main():
             a['silver_labels'] = labels
 
         # Save data
+        if GLOBAL_NORMALIZE:
+            mode = mode + '_global'
         utils.save_json(annotated_train_data, config.PATH_DATA, 'train_labeled_data_' + mode + '.json')
         utils.save_json(annotated_dev_data, config.PATH_DATA,  'dev_labeled_data_' + mode + '.json')
 
--- analyze_aggregation.py
+++ analyze_aggregation.py
@@ -1,81 +1,27 @@
 # Settings
-import aggregation
-import metrics
-
 exclude = []
 # exclude all hard labeling functions
 #exclude = ['LF_lemma_match_without_stopwords', 'LF_pos_match_without_stopwords', 'LF_dep_match', 'LF_pos_match', 'LF_tag_match', 'LF_bleu_candidates','LF_edit_distance', 'LF_jaccard_similarity']
 # exclude functions with low performance
 #exclude = ['LF_pos_match', 'LF_tag_match', 'LF_dep_match','LF_pos_match_without_stopwords','LF_edit_distance', 'LF_jaccard_similarity']
-TOKENIZER_NAME = 'distilroberta-base'
 th = 0.5
-GLOBAL_NORMALIZE = False
+GLOBAL_NORMALIZE = True
 average_outliers = False
 
-
-# Generate filename
-results_name = 'results_global_normalize' if GLOBAL_NORMALIZE else 'results'
-results_name += '_average_outliers' if average_outliers else ''
-
 #Imports
-import json
-import spacy
-from transformers import AutoTokenizer
+import metrics
 import numpy as np
 import pandas as pd
-from spacy import displacy
 import config
-import matplotlib.pyplot as plt
 import skweak
 import myutils as utils
 
-# Functions
-def global_normalize(data, average_outliers=False):
-    # finding min and max value
-    min_value = 100
-    max_value = -100
-    max_vals = []
-    min_vals = []
-    for d in data:
-        v_min = np.min(d)
-        v_max = np.max(d)
-        if v_min < min_value:
-            min_value = v_min
-            min_vals.append(min_value)
-        if v_max > max_value:
-            max_value = v_max
-            max_vals.append(max_value)
-    #print('max', max_vals, 'min', min_vals)
-    #min_value = min_vals[0]
-    #max_value = max_vals[0]
-    if average_outliers:
-        min_value = np.average(min_vals)
-        max_value = np.average(max_vals)
-    range_val = max_value - min_value
-    data_norm = [[(l - min_value) / range_val for l in d] for d in data]
-    return data_norm
-
-
-def normalize(sequence):
-    min_value = min(sequence)
-    max_value = max(sequence)
-    range_value = max_value - min_value
-    if range_value == 0:
-        range_value = 0.001
-    normalized_sequence = [(x - min_value) / range_value for x in sequence]
-    return normalized_sequence
-
-def plot_bars(classes, data, title):
-    plt.bar(classes, data)
-    plt.title(title)
-    plt.show()
-
-# Loading
 def main():
     results = []
     german_question_ids = [str(i) for i in range(1, 10)]
     files = ['train_labeled_data_hmm.json', 'train_labeled_data_sum.json', 'train_labeled_data_average.json', 'train_labeled_data_max.json', 'train_labeled_data_average_nonzero.json']
-    rubrics = utils.load_rubrics(config.PATH_DATA + '/' + 'rubrics.json')
+    if GLOBAL_NORMALIZE:
+        files = ['train_labeled_data_hmm.json', 'train_labeled_data_sum_global.json', 'train_labeled_data_average_global.json', 'train_labeled_data_max_global.json', 'train_labeled_data_average_nonzero_global.json']
     for file in files:
         print('Analyzing.. ' + file)
         disable_lang_filter = False
@@ -165,11 +111,16 @@ def main():
             doc.append(tokens)
 
         results_name = file.split('.')[0]
+        if GLOBAL_NORMALIZE:
+            results_name += '_global'
         skweak.utils.docbin_writer(doc, 'corpora/' + results_name + '.spacy')
         print('saved to disk')
 
     results = pd.DataFrame(columns=results[0].keys(), data=results)
-    results.to_csv('results/results.csv', index=False)
+    result_file_name = 'results'
+    if GLOBAL_NORMALIZE:
+        result_file_name += '_global'
+    results.to_csv('results/' + result_file_name + '.csv', index=False)
 
 if __name__ == '__main__':
     main()
