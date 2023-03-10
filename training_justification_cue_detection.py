1535e9c0b9d4cf2e4e551fb48a9c8bd6bd2fbee2
chefkoch24 <kuennecke-felix@web.de>
2023-03-10 18:19:44 +0100

    optimize training scripts

--- training_justification_cue_detection.py
+++ training_justification_cue_detection.py
@@ -2,6 +2,9 @@
 import argparse
 from pytorch_lightning.loggers import CSVLogger
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+from tqdm import tqdm
+from transformers import AutoTokenizer
+
 from model import TokenClassificationModel
 from torch.utils.data import DataLoader
 from pytorch_lightning import Trainer
@@ -14,33 +17,57 @@ from dataset import JustificationCueDataset
 import warnings
 warnings.filterwarnings("ignore")
 
-parser=argparse.ArgumentParser()
-parser.add_argument("--model", type=str, help="Name of the pretrained model", default="distilbert-base-multilingual-cased")
-parser.add_argument("--train_file", type=str, help="train file")
-parser.add_argument("--dev_file", type=str,help="dev file")
-parser.add_argument("--test_file", type=str, help="test file", default=None)
-parser.add_argument("--context", type=bool, help="with context or not", default=False)
-args=parser.parse_args()
+def pre_process(data, with_context=False):
+    model_inputs = []
+    for d in tqdm(data):
+        student_answer = d['student_answer']
+        # Tokenize the input
+        if with_context:
+            context = d['reference_answer']
+            tokenized = tokenizer(student_answer, context, max_length=config.MAX_LEN, truncation=True, padding='max_length', return_token_type_ids=True)
+        else:
+            tokenized = tokenizer(student_answer, max_length=config.MAX_LEN, truncation=True, padding='max_length', return_token_type_ids=True)
+        # Generating the labels
+        aligned_labels = d['aligned_labels']
+        pad_len = config.MAX_LEN - len(aligned_labels) -2
+        labels = [-100]+aligned_labels + [-100]
+        # Adding other model inputs
+        model_input = {
+            'input_ids': tokenized['input_ids'],
+            'attention_mask': tokenized['attention_mask'],
+            'token_type_ids': tokenized['token_type_ids'],
+            'labels': utils.create_labels_probability_distribution(torch.nn.functional.pad(torch.tensor(labels), pad=(0, pad_len), mode='constant', value=-100).detach().numpy().tolist()),
+            'class': d['label'],
+            'question_id': d['question_id'],
+            'student_answer': d['student_answer'],
+            'reference_answer': d['reference_answer'],
+        }
+        model_inputs.append(model_input)
+
+    return model_inputs
 
 #Set seed
 torch.manual_seed(config.SEED)
 torch.cuda.manual_seed_all(config.SEED)
-
-
 # Load data
-training_data = utils.load_json(config.PATH_DATA + '/' + args.train_file)
-dev_data = utils.load_json(config.PATH_DATA + '/' + args.dev_file)
+train_data = utils.load_json(config.PATH_DATA + '/' + config.ALIGNED_TRAIN_FILE)
+dev_data = utils.load_json(config.PATH_DATA + '/' + config.ALIGNED_DEV_FILE)
 rubrics = utils.load_rubrics(config.PATH_RUBRIC)
+tokenizer = AutoTokenizer.from_pretrained(config.MODEL_NAME)
 
-training_dataset = JustificationCueDataset(training_data[0:8])
-dev_dataset = JustificationCueDataset(dev_data[0:8])
+# Preprocess data
+training_dataset = pre_process(train_data, with_context=config.CONTEXT)
+dev_dataset = pre_process(dev_data, with_context=config.CONTEXT)
+
+training_dataset = JustificationCueDataset(training_dataset)
+dev_dataset = JustificationCueDataset(dev_dataset)
 # Training
 train_loader = DataLoader(training_dataset, batch_size=config.BATCH_SIZE, shuffle=True)
 val_loader = DataLoader(dev_dataset, batch_size=config.BATCH_SIZE, shuffle=False)
-model = TokenClassificationModel(args.model)
+model = TokenClassificationModel(config.MODEL_NAME)
 
 
-EXPERIMENT_NAME = "justification_cue" + "_" + args.model + "_context-" + str(args.context)
+EXPERIMENT_NAME = "justification_cue" + "_" + config.MODEL_NAME + "_context-" + str(config.CONTEXT) + "_bs-" + str(config.BATCH_SIZE)
 logger = CSVLogger("logs", name=EXPERIMENT_NAME)
 trainer = Trainer(max_epochs=config.NUM_EPOCHS,
                   #gradient_clip_val=0.5,
--- training_span_prediction.py
+++ training_span_prediction.py
@@ -1,33 +1,77 @@
-import argparse
-
+import numpy as np
 import torch
 from pytorch_lightning.loggers import CSVLogger
 from pytorch_lightning import Trainer
 from torch.utils.data import DataLoader
-
+from tqdm import tqdm
+from transformers import AutoTokenizer
+import metrics
 import myutils as utils
 import config
 from dataset import SpanJustificationCueDataset
 from model import SpanPredictionModel
+from paraphrase_scorer import BertScorer
+
+
+def get_rubric_elements(spans, input_ids, qid):
+    rubric = rubrics[qid]
+    rubric_elements = []
+    sims = []
+    for s in spans:
+        span_text = tokenizer.decode(input_ids[s[0]:s[1]])
+        sim = para_detector.detect_score_key_elements(span_text, rubric)
+        max_index = np.argmax(sim)
+        rubric_element = rubric['key_element'][max_index]
+        rubric_elements.append(rubric_element)
+        sims.append(np.max(sim))
+    return rubric_elements, sims
+
+
+def create_inputs(data):
+    model_inputs = []
+    for d in tqdm(data):
+        student_answer = d['student_answer']
+        aligned_labels = d['aligned_labels']
+        q_id = d['question_id']
+        # Tokenize the input to generate alignment
+        tokenized = tokenizer(student_answer, add_special_tokens=False, return_offsets_mapping=True)
+        # get the spans from the aligned labels
+        hard_labels = metrics.silver2target(aligned_labels)
+        spans = metrics.get_spans_from_labels(hard_labels)
+        rubric_elements, rubric_sims = get_rubric_elements(spans, tokenized['input_ids'], q_id)
+        # generate final input for every span individually
+        for span, re, sim in zip(spans, rubric_elements, rubric_sims):
+            tokenized = tokenizer(student_answer, re, max_length=config.MAX_LEN, truncation=True, padding='max_length', return_token_type_ids=True)
+            model_input = {
+                'input_ids': tokenized['input_ids'],
+                'attention_mask': tokenized['attention_mask'],
+                'token_type_ids': tokenized['token_type_ids'],
+                'start_positions': [span[0]],
+                'end_positions': [span[1]],
+                'question_id': q_id,
+                'rubric_element': re,
+                'class': d['label'],
+                'similarity': sim,
+                'student_answer': student_answer
+            }
+            model_inputs.append(model_input)
+    return model_inputs
+
 
 #Set seed
 torch.manual_seed(config.SEED)
 torch.cuda.manual_seed_all(config.SEED)
-
-parser=argparse.ArgumentParser()
-parser.add_argument("--model", type=str, help="Name of the pretrained model", default="distilbert-base-multilingual-cased")
-parser.add_argument("--train_file", type=str, help="train file")
-parser.add_argument("--dev_file", type=str,help="dev file")
-parser.add_argument("--test_file", type=str, help="test file", default=None)
-args=parser.parse_args()
-
-args.train_file = 'training_dataset_span_prediction_distilbert-base-multilingual-cased.json'
-args.dev_file= 'dev_dataset_span_prediction_distilbert-base-multilingual-cased.json'
+#Loading
+train_data = utils.load_json(config.PATH_DATA + '/' + config.ALIGNED_TRAIN_FILE)
+dev_data = utils.load_json(config.PATH_DATA + '/' + config.ALIGNED_DEV_FILE)
+tokenizer = AutoTokenizer.from_pretrained(config.MODEL_NAME)
+rubrics = utils.load_rubrics(config.PATH_RUBRIC)
+para_detector = BertScorer()
 
 
 # Load data
-training_data = utils.load_json(config.PATH_DATA + '/' + args.train_file)
-dev_data = utils.load_json(config.PATH_DATA + '/' + args.dev_file)
+training_data = utils.load_json(config.PATH_DATA + '/' + config.ALIGNED_TRAIN_FILE)
+dev_data = utils.load_json(config.PATH_DATA + '/' + config.ALIGNED_DEV_FILE)
 rubrics = utils.load_rubrics(config.PATH_RUBRIC)
 
 training_dataset = SpanJustificationCueDataset(training_data)
@@ -35,9 +79,9 @@ dev_dataset = SpanJustificationCueDataset(dev_data)
 # Training
 train_loader = DataLoader(training_dataset, batch_size=config.BATCH_SIZE, shuffle=True)
 val_loader = DataLoader(dev_dataset, batch_size=config.BATCH_SIZE, shuffle=False)
-model = SpanPredictionModel(args.model)
+model = SpanPredictionModel(config.MODEL_NAME)
 
-EXPERIMENT_NAME = "span_prediction" + "_" + config.MODEL_NAME
+EXPERIMENT_NAME = "span_prediction" + "_" + config.MODEL_NAME + "_batch_" + str(config.BATCH_SIZE)
 logger = CSVLogger("logs", name=EXPERIMENT_NAME)
 trainer = Trainer(max_epochs=config.NUM_EPOCHS,
                   #gradient_clip_val=0.5,
