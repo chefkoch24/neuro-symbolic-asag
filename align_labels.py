bd01027ac81e48e410ef9f35cf8daf8ea1b4a27d
chefkoch24 <kuennecke-felix@web.de>
2023-03-14 21:06:53 +0100

    align labels in one file

--- align_labels.py
+++ align_labels.py
@@ -1,9 +1,27 @@
+import numpy as np
+import tokenizations
 from tqdm import tqdm
 from transformers import AutoTokenizer
 
 import config
 import myutils as utils
-import utils_preprocessing
+
+def align_generate_labels_all_tokens(tokens_spacy, tokens_bert, l):
+    a2b, b2a = tokenizations.get_alignments(tokens_spacy, tokens_bert)
+    len_of_classification = len(tokens_bert)  # for CLS and end of seq
+    label_ids = np.zeros((len_of_classification))
+    previous_label_idx = 0
+    label_idx = -1
+    for j, e in enumerate(b2a):
+        if len(e) >= 1:  # Not special token
+            label_idx = e[0]
+            # if label_idx < len_of_classification:
+            label_ids[j] = l[label_idx]
+            previous_label_idx = label_idx
+        else:
+            label_ids[j] = l[previous_label_idx]
+    # label_ids[len_of_classification:] = -100
+    return label_ids
 
 
 def create_aligned_labels(data):
