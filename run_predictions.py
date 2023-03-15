c45b38dd99bc455dd37d40f04de791cdc2c52978
chefkoch24 <kuennecke-felix@web.de>
2023-03-15 23:01:27 +0100

    span predictions bug fixed

--- run_predictions.py
+++ run_predictions.py
@@ -3,12 +3,19 @@ import torch
 from config import Config
 from prediction import PredictSpan, PredictToken
 
+# config token_classification
 TASK = 'token_classification'
 CHECKPOINT_PATH = 'logs/token_classification_distilbert-base-multilingual-cased_bs-8_aggr-lfs_sum_context-True/version_0/checkpoints/checkpoint-epoch=00-val_loss=0.67.ckpt'
 MODEL = 'distilbert-base-multilingual-cased'
 CONTEXT = True
 TEST_FILE = 'dev_dataset_aligned_labels_distilbert-base-multilingual-cased_lfs_sum.json'
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# config span_prediction
+TASK = 'span_prediction'
+CHECKPOINT_PATH = 'logs/span_prediction_distilbert-base-multilingual-cased_bs-8_aggr-lfs_sum/version_0/checkpoints/checkpoint-epoch=03-val_loss=3.24.ckpt'
+MODEL = 'distilbert-base-multilingual-cased'
+TEST_FILE = 'dev_dataset_aligned_labels_distilbert-base-multilingual-cased_lfs_sum.json'
 
 if TASK == 'span_prediction':
     config = Config(task='span_prediction',
